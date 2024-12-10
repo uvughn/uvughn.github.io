f1c237eb88a5ac780a07c0b7fda7b6b5b946fbf0
uvughn <108960069+uvughn@users.noreply.github.com>
2024-12-10 00:49:52 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const handleClick = (name) => {
+  location.href = `http://127.0.0.1:5500/YES_YOGA_PROJECT/YES%20YOGA%20PROJECT/${name}.html`;
+};
+
+const data = [
+  {
+    name: "Tina",
+    description: "Tina guides you to find balance and strength in every pose",
+    image: "tina2.avif", // image for Tina
+  },
+  {
+    name: "Amy",
+    description: "Amy inspires mindfulness and flexibility in her yoga practice",
+    image: "amy.avif", // image for Amy
+  },
+  {
+    name: "Crystal",
+    description: "Crystal helps you connect your body, mind, and breath",
+    image: "crystal.avif", // image for Crystal
+  },
+];
+
+let str = "";
+for (let i in data) {
+  str += `<div class="card">
+                <img src="${data[i].image}" alt="${data[i].name}">
+                <h2>${data[i].name}</h2>
+                <p>${data[i].description}</p>
+                <button onclick="handleClick('${data[i].name.toLowerCase()}')">Book a session</button>
+            </div>`;
+}
+
+const div = document.getElementById("cards");
+// console.log(div.innerHTML);
+div.innerHTML = str;
